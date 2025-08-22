2f0e0d39ecda4f01239048d48621c3fe1b583bae
jtmse78-hub <jtmse78@gmail.com>
2025-08-22 15:57:19 +0800

    first commit

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\TemplateController;
+
+class TemplateController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.home');
+    }
+}
